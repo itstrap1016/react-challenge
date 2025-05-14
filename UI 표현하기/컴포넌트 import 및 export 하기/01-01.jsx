fed7e8921342ff70b2134e5d62bb73a4a8c209d6
이한승 <itstrap1016@gmail.com>
2025-05-14 10:32:55 +0900

    컴포넌트 import 및 export 하기

---
+++ UI 표현하기/컴포넌트 import 및 export 하기/01-01.jsx
@@ -0,0 +1,11 @@
+import Gallery from "./01-02";
+import Profile from "./01-03";
+
+export default function App() {
+  return (
+    <div>
+      <Gallery />
+      <Profile />
+    </div>
+  );
+}
---
+++ UI 표현하기/컴포넌트 import 및 export 하기/01-02.jsx
@@ -0,0 +1,12 @@
+import Profile from "./01-03";
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
